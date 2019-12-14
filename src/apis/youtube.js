bf44ca739e9d24c133ed3605d8b96a6015499b24
Sahil Naikdhure <sanaikdhure@gmail.com>
2019-12-14 09:35:01 +0530

    adds axios base config

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyDQ5gFwW2nl1x80v4lCCs79sgNFfpYJ8Y0';
+
+export default axios.create({
+    baseURL : 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResult: 5,
+        key: KEY
+    }
+})
