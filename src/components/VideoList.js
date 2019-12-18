7900c99108078bff1cfea6f9beb96884f4296537
Sahil Naikdhure <sanaikdhure@gmail.com>
2019-12-18 15:24:04 +0530

    adds video list and item component

---
+++ src/components/VideoList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import VideoItem from './VideoItem'
+
+const VideoList = ({videos}) => {
+    return videos.map((video) => {
+        return <VideoItem video ={video}/>
+    })
+}
+
+export default VideoList;
